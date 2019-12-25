8b4e014862cd899c4fa7f9a4d06b4f29f68dc95d
0xBAMA <jon.baker1222@gmail.com>
2019-12-25 11:23:25 -0500

    progress

--- resources/shaders/phong_vs.glsl
+++ resources/shaders/phong_vs.glsl
@@ -1,17 +1,26 @@
 #version 450
 
-in vec3 i_position;   //input position
-in vec3 i_normal;     //input normal
+in vec3 i_position;               //input position
+in vec3 i_normal;                 //input normal
 
-out vec3 v_normal;    //normal with any rotations applied
-out vec4 v_color;     //output color (will be interpolated across primitive)
+out vec3 v_normal;                //normal with any rotations applied
+out vec4 v_color;                 //output color (will be interpolated across primitive)
 
-uniform mat4 u_projection_matrix;
-uniform mat4 u_view_matrix;
+uniform mat4 u_projection_matrix; //perspective projection matrix
+uniform mat4 u_view_matrix;       //made with glm::lookat()
+
+uniform vec4 u_color;              //allows separate colors per piece
+uniform vec3 u_offset;             //allows for placement of pieces
+
+uniform float rotation;            //allows the user to rotate the board
 
 void main()
 {
-    v_color = vec4(0,0,0,1);
+    v_color = u_color;              //passthrough
+    v_normal = i_normal;            //passthrough
+
+    //normal                        //make sure to transform normal along with the point
+
+    //have to add the offset, before model(rotation),view,projection
     gl_Position = u_projection_matrix*vec4( i_position, 1.0 );
-    //gl_Position = vec4(0,0,0,1);
 }
