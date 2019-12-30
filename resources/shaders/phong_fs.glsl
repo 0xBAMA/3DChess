95f1469cae4dc8367fa69a0c717caf4c42c39f56
0xBAMA <jon.baker1222@gmail.com>
2019-12-30 15:44:08 -0500

    two lights

--- resources/shaders/phong_fs.glsl
+++ resources/shaders/phong_fs.glsl
@@ -30,7 +30,8 @@ uniform float light_rotation;
 
 void main()
 {
-  vec3 light = (rotation(vec3(0,1,0),-0.001*light_rotation)*vec4(0.2, 0.5+0.1*sin(0.01*light_rotation), 0.0, 1)).xyz;
+  vec3 light = (rotation(vec3(0,1,0),-0.001*light_rotation)*vec4(0.2, 0.7+0.3*sin(0.01*light_rotation), 0.0, 1)).xyz;
+  vec3 light2 = (rotation(vec3(0,1,0),-0.001*light_rotation)*vec4(-0.2, 0.7+0.3*sin(0.01*light_rotation), 0.0, 1)).xyz;
 
   o_color = v_color;
 
@@ -58,6 +59,12 @@ void main()
     vec3 l = normalize(v_position - light);
     vec3 v = normalize(v_position - eye);
     vec3 n;
+
+    vec3 l2 = normalize(v_position - light2);
+    vec3 v2 = normalize(v_position - eye);
+    vec3 n2;
+
+
     // if(gl_FrontFacing)
     // {
     //   n = normalize(v_normal);
@@ -69,22 +76,32 @@ void main()
     //   if(mode == 1 || mode == 2)
     //     n = normalize(v_normal);
     // }
+
+
     vec3 r = normalize(reflect(l, n));
+    vec3 r2 = normalize(reflect(l2, n2));
 
     //ambient
     float a = -0.4;
     //diffuse
-    float d = (1/(pow(0.25*distance(v_position,light),2))) * 0.3 * max(dot(n, l),-0.4);
+    float d = (1/(pow(0.25*distance(v_position,light),2))) * 0.2 * max(dot(n, l),-0.4);
+    float d2 = (1/(pow(0.25*distance(v_position,light2),2))) * 0.2 * max(dot(n2, l2),-0.4);
     //specular
-    float s = (1/(pow(0.25*distance(v_position,light),2))) * 0.4 * pow(max(dot(r,v),0),100);
+    float s = (1/(pow(0.25*distance(v_position,light),2))) * 0.25 * pow(max(dot(r,v),0),100);
+    float s2 = (1/(pow(0.25*distance(v_position,light2),2))) * 0.25 * pow(max(dot(r2,v2),0),100);
     // s = clamp(s, 0, 2);
 
     o_color.xyz += a * vec3(0.4,0.4,0.4);
     o_color.xyz += d * vec3(0.56, 0.3,0.1);
+    o_color.xyz += d2 * vec3(0.56, 0.3,0.1);
     if(dot(n,l) > 0)
+    {
       o_color.xyz += s * vec3(1,0.0,0);
+      o_color.xyz += s2 * vec3(1,0.0,0);
+    }
 
-    o_color.xyz *= 0.5/gl_FragCoord.z;
+    // o_color.xyz /= 0.5*gl_FragCoord.z;
+    o_color.b*=0.3;
   }
 
 
